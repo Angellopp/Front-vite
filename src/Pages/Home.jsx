c29a7ce9c60f7b81eec09a55fa4d14fcc9c602b5
Piero Pisfil <piero.pisfil.p@uni.pe>
2024-02-19 19:26:23 -0500

    add target

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -29,6 +29,16 @@ function Home() {
             <h1 className="font-black text-5xl text-center mb-10">Bienvenido!</h1>
             <h2 className="font-black text-4xl text-center mb-10">Esta es la home page.</h2>
 
+            {loggedIn ? 
+            <div>
+                <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mx-auto">
+
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                </a>
+            </div>
+            : <div/>}
+
             <div className="container mx-auto mt-20 mb-20">
                 <div className="text text-center">
                     {loggedIn ?
