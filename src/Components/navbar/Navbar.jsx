39a04ef5973f94d613d22cf4b81cce44ebb7dbc0
Piero Pisfil <piero.pisfil.p@uni.pe>
2024-04-25 19:31:04 -0500

    viendo el forntend

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -70,9 +70,9 @@ export default function Navbar() {
                   >
                     <path
                       stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="m1 1 4 4 4-4"
                     />
                   </svg>
@@ -111,9 +111,9 @@ export default function Navbar() {
                         >
                           <path
                             stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
                             d="m1 1 4 4 4-4"
                           />
                         </svg>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,9 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import useVerifyToken from "../Api/useVerifyToken";
 import useLogout from "../hooks/auth/useLogout";
-import Navbar from "../Components/navbar/Navbar";
+// import Navbar from "../Components/navbar/Navbar";
+// import Sidebar from "../Components/navbar/Sidebar";
+import MyDrawer from "../Components/navbar/MyDrawer";
 
 function Home() {
     const logout = useLogout();
@@ -26,50 +28,54 @@ function Home() {
     }
 
     return (
-        <div className="container mx-auto mt-20 mb-20">
-            <Navbar/>
-            <h1 className="font-black text-5xl text-center mb-10">Bienvenido!</h1>
-            <h2 className="font-black text-4xl text-center mb-10">Esta es la home page.</h2>
+        <div>
+            {/* <Sidebar/> */}
+            <MyDrawer/>
+            {/* <Navbar/> */}
+            <div className="container mx-auto mt-20 mb-20">
+                <h1 className="font-black text-5xl text-center mb-10">Bienvenido!</h1>
+                <h2 className="font-black text-4xl text-center mb-10">Esta es la home page.</h2>
 
-            {isLoading ? 
-                <div className="text text-center text-3xl"> Cargando... </div>
-            : <div/>}
+                {isLoading ? 
+                    <div className="text text-center text-3xl"> Cargando... </div>
+                : <div/>}
 
-            {loggedIn ? 
-            <div>
-                <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mx-auto">
+                {loggedIn ? 
+                <div>
+                    <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mx-auto">
 
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                </a>
-            </div>
-            : <div/>}
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                    <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    </a>
+                </div>
+                : <div/>}
 
-            <div className="container mx-auto mt-10 mb-10">
-                <div className="text text-center">
-                    {loggedIn ?
-                        <button
-                            type="submit"
-                            className="mx-auto relative inline-flex items-center justify-center p-0.5 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800"
-                            onClick={goDashboard}
-                        >
-                            <span className="mx-auto relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                                Ir al Dashboard
-                            </span>
-                        </button>
-                        : <div />
-                    }
-                    {isSuccess ? 
-                        <button
-                            type="submit"
-                            className="mx-auto relative inline-flex items-center justify-center p-0.5 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800"
-                            onClick={onButtonClick}
-                        >
-                            <span className="mx-auto relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                                {loggedIn ? "Log out" : "Log in"}
-                            </span>
-                        </button>
-                    : <div/>}
+                <div className="container mx-auto mt-10 mb-10">
+                    <div className="text text-center">
+                        {loggedIn ?
+                            <button
+                                type="submit"
+                                className="mx-auto relative inline-flex items-center justify-center p-0.5 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800"
+                                onClick={goDashboard}
+                            >
+                                <span className="mx-auto relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                    Ir al Dashboard
+                                </span>
+                            </button>
+                            : <div />
+                        }
+                        {isSuccess ? 
+                            <button
+                                type="submit"
+                                className="mx-auto relative inline-flex items-center justify-center p-0.5 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800"
+                                onClick={onButtonClick}
+                            >
+                                <span className="mx-auto relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                    {loggedIn ? "Log out" : "Log in"}
+                                </span>
+                            </button>
+                        : <div/>}
+                    </div>
                 </div>
             </div>
         </div>
