1de5cbf7a4ea57ee4b8674cdd2f1b1adc7f1e284
Piero Pisfil <piero.pisfil.p@uni.pe>
2024-07-13 20:07:25 -0500

    cvbv

--- src/Components/popover/PopoverStock.jsx
+++ src/Components/popover/PopoverStock.jsx
@@ -48,6 +48,7 @@ export default function PopoverStock({ dataToPopover }) {
                             productStock.result.map((stock, index) => (
                                 <p key={index} className="text-base font-semibold leading-none text-gray-900 dark:text-white mb-1">
                                     {`[${stock.location_id[1]}]: ${stock.quantity} ${stock.product_uom_id[1]}`}
+                                    <button data-popover-target="popover-description" data-popover-placement="bottom-end" type="button"><svg className="w-4 h-4 ms-2 text-gray-400 hover:text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd"></path></svg><span className="sr-only">Show information</span></button>
                                 </p>
                             ))
                         ) : (
