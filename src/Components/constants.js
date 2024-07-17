1fc14d4d55bc1f3b3803efbd221d096236d995d5
Angellopp <angellojamir@hotmail.com>
2024-07-17 14:27:10 -0500

    ADD new filters

--- src/Components/constants.js
+++ src/Components/constants.js
@@ -6,7 +6,9 @@ export const filterProducts = (products, value) => {
   return products
     ? value
       ? products.filter((item) =>
-          item.name.toLowerCase().includes(value.toLowerCase())
+          item.name.toLowerCase().includes(value.toLowerCase()) ||
+          item.description_sale?.toString().toLowerCase().includes(value.toLowerCase()) ||
+          item.default_code?.toString().toLowerCase().includes(value.toLowerCase())
         )
       : products
     : [];
