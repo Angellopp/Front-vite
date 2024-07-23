f054badfc8057271f9780ca2a55b588c7a979a41
Piero Pisfil <piero.pisfil.p@uni.pe>
2024-07-23 13:47:57 -0500

    card date

--- src/Components/card/CardActivity.jsx
+++ src/Components/card/CardActivity.jsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { Card, Kbd } from "flowbite-react";
+import { Card, Kbd, Toast } from "flowbite-react";
 import useActivityEmail from "../../hooks/activitiesEmail/useActivityEmail";
+import { BiCalendarExclamation } from "react-icons/bi";
 
 export default function CardsActivity() {
   const { data: activityEmails } = useActivityEmail(
@@ -15,48 +16,47 @@ export default function CardsActivity() {
     <>
       {activityEmails && activityEmails.length > 0 ? (
         <div className="text text-center text-xl dark:text-white">
-          Hay{" "}
-            <Kbd>{activityEmails.length}</Kbd>
-          {" "}
-          notificaciones
+          Hay <Kbd>{activityEmails.length}</Kbd> notificaciones
         </div>
       ) : (
         <div />
       )}
-      <Card href="#" className="max-w-sm">
-        {/* // <div>
-                //     <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mx-auto">
-
-                //     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                //     <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                //     </a>
-                // </div>
-                // : <div/> */}
+      <div className="grid gap-12 p-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 overflow-x-auto shadow-md sm:rounded-lg">
         {activityEmails && activityEmails.length > 0 ? (
           activityEmails.map((email) => (
-            <div className="text text-center text-3xl" key={email.id}>
-              <div className="text text-center text-3xl">
-                {" "}
-                {email.res_name}{" "}
-              </div>
-              <div className="text text-center text-3xl">
-                {" "}
-                {email.display_name}{" "}
-              </div>
-              Deadline: {email.date_deadline}
+            <div className="text text-center" key={email.id}>
+              <Card className="max-w-sm">
+                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  {email.res_name}
+                </h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                  {email.note}
+                </p>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                  {email.display_name}
+                  {email.activity_type_id[1]}
+                </p>
+                <Toast className="bg-red-100 dark:bg-red-900">
+                  <BiCalendarExclamation className="h-5 w-5 text-cyan-600 dark:text-cyan-500" />
+                  <div className="pl-4 text-sm font-normal">
+                    {email.date_deadline}
+                  </div>
+                </Toast>
+              </Card>
             </div>
           ))
         ) : (
-          <div />
+          <div>
+            <div className="text text-center text-3xl">
+              <Card className="max-w-sm">
+                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  No hay notificaciones
+                </h5>
+              </Card>
+            </div>
+          </div>
         )}
-        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          Noteworthy technology acquisitions 2021
-        </h5>
-        <p className="font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
-        </p>
-      </Card>
+      </div>
     </>
   );
 }
